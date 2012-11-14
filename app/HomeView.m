0402b827204739df0e432180ac02fdf32ff05c67
Jazzy <jaskaransingh3001@gmail.com>
2012-11-13 20:05:45 -0800

    added home screen

---
+++ app/HomeView.m
@@ -0,0 +1,13 @@
+//
+//  HomeView.m
+//  app
+//
+//  Created by App Jam on 11/13/12.
+//  Copyright (c) 2012 Team 14. All rights reserved.
+//
+
+#import "HomeView.h"
+
+@implementation HomeView
+
+@end
---
+++ app/app/CCHPIViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCHPIViewController.h
+//  app
+//
+//  Created by App Jam on 11/13/12.
+//  Copyright (c) 2012 Team 14. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCHPIViewController : UIViewController
+
+@end
