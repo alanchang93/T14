3cf0acd28efd5949434f8ed549da74d7cbe98aa8
Jazzy <jaskaransingh3001@gmail.com>
2012-11-13 20:09:31 -0800

    Revert "Added all the values to the popover"
    This reverts commit b0576afb0480713b3a36afb556a09c9c233a75c9.

---
+++ app/HomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.h
+//  app
+//
+//  Created by App Jam on 11/13/12.
+//  Copyright (c) 2012 Team 14. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+- (IBAction)newPatientButton:(id)sender;
+
+@end
